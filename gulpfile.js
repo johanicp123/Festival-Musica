431d1c5066ceb61dbf5758ef0566a36e02fea645
johanicp123 <johanicp123@gmail.com>
2022-05-12 04:26:47 -0500

    antepenultima actualizacion proyecto
    se agrega mas sass,javascript y html al proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,48 @@
-const {src,dest,watch} = require("gulp");
+const {src,dest,watch, parallel} = require("gulp");
+
+//csss
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function imagenes(done){
+    const opciones= {
+        optimizationLevel:3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp(done){
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+
+function versionAvif(done){
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
 function css(done){
    src('src/scss/**/*.scss') //identificar el archivo de sass
     .pipe(plumber())
@@ -10,9 +52,22 @@ function css(done){
     done();//callbackque avsa a gulp cuando llegamos al final
 }
 
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev(done){
-    watch('src/scss/**/*.scss',css)
+    watch('src/scss/**/*.scss',css);
+    watch('src/js/**/*.js',javascript);
+    done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev =parallel(imagenes,versionWebp,versionAvif,javascript,dev);
